47bab7189005f6c3f06ed1350fb44fe51fe01cbb
Thiago Marinho <thiagomarinhobezerra@gmail.com>
2025-06-19 22:16:11 -0300

    feat: list team by id

--- src/main/java/com/basket/api/modules/Team/controller/TeamController.java
+++ src/main/java/com/basket/api/modules/Team/controller/TeamController.java
@@ -2,13 +2,13 @@ package com.basket.api.modules.Team.controller;
 
 import com.basket.api.modules.Team.entity.TeamEntity;
 import com.basket.api.modules.Team.useCases.CreateTeamUseCase;
+import com.basket.api.modules.Team.useCases.ListTeamUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/teams")
@@ -17,10 +17,23 @@ public class TeamController {
     @Autowired
     private CreateTeamUseCase createTeamUseCase;
 
+    @Autowired
+    private ListTeamUseCase listTeamUseCase;
+
     @PostMapping("/")
     public ResponseEntity<Object> createTeam(@Valid @RequestBody TeamEntity teamEntity) {
         try {
             var result = this.createTeamUseCase.execute(teamEntity);
+            return ResponseEntity.ok().body(result);
+        }catch(Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TeamEntity> getTeam(@PathVariable UUID id) {
+        try{
+            var result =  this.listTeamUseCase.execute(id);
             return ResponseEntity.ok().body(result);
         }catch(Exception e) {
             return ResponseEntity.badRequest().build();
