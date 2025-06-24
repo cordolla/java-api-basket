b1e92c1f3f2c9d860b8f5f9fa3605af923404807
Thiago Marinho <thiagomarinhobezerra@gmail.com>
2025-06-24 10:53:58 -0300

    feat: list game by league id

--- src/main/java/com/basket/api/modules/game/repository/GameRepository.java
+++ src/main/java/com/basket/api/modules/game/repository/GameRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 import java.util.UUID;
 
 public interface GameRepository extends JpaRepository<GameEntity, UUID> {
-    List<GameEntity> listGameByLeagueId(UUID leagueId)
+    List<GameEntity> findByLeagueId(UUID leagueId);
 }
--- src/main/java/com/basket/api/modules/game/useCases/ListGameByLeagueIdUseCase.java
+++ src/main/java/com/basket/api/modules/game/useCases/ListGameByLeagueIdUseCase.java
@@ -1,8 +1,13 @@
 package com.basket.api.modules.game.useCases;
 
+import com.basket.api.modules.game.entity.GameEntity;
 import com.basket.api.modules.game.repository.GameRepository;
+import com.basket.api.modules.league.repository.LeagueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ListGameByLeagueIdUseCase {
@@ -10,7 +15,7 @@ public class ListGameByLeagueIdUseCase {
     @Autowired
     private GameRepository gameRepository;
 
-    public ListGameByLeagueIdUseCase(GameRepository gameRepository) {
-
+    public List<GameEntity> execute(UUID leagueId){
+        return gameRepository.findByLeagueId(leagueId);
     }
 }
