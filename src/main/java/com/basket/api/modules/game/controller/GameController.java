bf6a9d1c44c70a425b2409e694ade516cc94e53a
Thiago Marinho <thiagomarinhobezerra@gmail.com>
2025-06-24 12:03:23 -0300

    feat: list games controller

--- src/main/java/com/basket/api/modules/game/controller/GameController.java
+++ src/main/java/com/basket/api/modules/game/controller/GameController.java
@@ -3,9 +3,13 @@ package com.basket.api.modules.game.controller;
 import com.basket.api.modules.game.dto.GameRequestDTO;
 import com.basket.api.modules.game.entity.GameEntity;
 import com.basket.api.modules.game.useCases.CreateGameUseCase;
+import com.basket.api.modules.game.useCases.ListGameByLeagueIdUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/games")
@@ -14,12 +18,25 @@ public class GameController {
     @Autowired
     private CreateGameUseCase createGameUseCase;
 
+    @Autowired
+    private ListGameByLeagueIdUseCase listGameByLeagueIdUseCase;
+
     @PostMapping
     public ResponseEntity<GameEntity> createGame(@RequestBody GameRequestDTO gameRequestDTO) {
         try {
             GameEntity game = createGameUseCase.execute(gameRequestDTO);
             return ResponseEntity.ok(game);
-        } catch (RuntimeException e) {
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    @GetMapping("/league/{id}")
+    public ResponseEntity<List<GameEntity>> listGamesByLeague(@PathVariable UUID id) {
+        try {
+            List<GameEntity> list = listGameByLeagueIdUseCase.execute(id);
+            return ResponseEntity.ok(list);
+        }catch (Exception e) {
             return ResponseEntity.badRequest().body(null);
         }
     }
