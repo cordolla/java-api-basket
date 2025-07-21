9efd378031f5a5c8a47f24f0b3f662990482c49f
Marcelo <cordolla10@gmail.com>
2025-07-21 19:25:11 -0300

    feat: update route security

--- src/main/java/com/basket/api/security/SecurityConfig.java
+++ src/main/java/com/basket/api/security/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/users/").permitAll().requestMatchers("/auth/**").permitAll();
+                    auth.requestMatchers("/users").permitAll().requestMatchers("/auth/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
