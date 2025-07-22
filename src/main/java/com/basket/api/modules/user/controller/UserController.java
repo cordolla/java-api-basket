bb6609e9be395cd53be4b330bb7065352e0af2db
Marcelo <cordolla10@gmail.com>
2025-07-21 22:06:27 -0300

    feat: adjust controller nomeclature

--- src/main/java/com/basket/api/modules/user/controller/UserController.java
+++ src/main/java/com/basket/api/modules/user/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
-public class UseController {
+public class UserController {
 
     @Autowired
     private CreateUserUseCase createUserUseCase;
